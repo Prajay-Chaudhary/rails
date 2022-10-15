3378ecf40ede993fed8bbeddc1d201fa69c593ac
Prajay Chaudhary <prajaychaudhary5678@gmail.com>
2022-10-15 14:39:19 +0200

    Add bcrypt and has_secure_password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ validates(:email,
           length: { maximum: 105 },
           # Verify email with ruby's standard library regex.
           format: { with: URI::MailTo::EMAIL_REGEXP })
+has_secure_password
 end
 
 
