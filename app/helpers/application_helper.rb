b8b0f662421ddd561b27bc5e07f405e1463060cf
Prajay Chaudhary <prajaychaudhary5678@gmail.com>
2022-10-15 19:53:16 +0200

    Add users create action

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,3 @@
 module ApplicationHelper
-  def form_error_notification(object)
-    if object.errors.any?
-      tag.div class: "alert alert-danger alert-dismissible fade show" do
-        object.errors.full_messages.each do |msg|
-          msg 
-        end
-      end
-    end
-  end
+
 end
